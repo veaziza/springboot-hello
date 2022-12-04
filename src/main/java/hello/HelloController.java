0153bd6787ed42c56acd960210434e8aef6f8203
Vincent Aziza <veaziza@yahoo.com>
2022-12-04 17:57:57 -0600

    hello

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Springboot.. Maven has fun in classes!!!";
+        return "Greetings from Springboot..!!!";
     }
     
 }
